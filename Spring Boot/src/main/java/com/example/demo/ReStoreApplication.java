2f23edd214f04568c98b468cc3fa95e102460434
Oleg <oleg.ptro737@gmail.com>
2021-04-11 17:03:39 +0300

    ...

---
+++ Spring Boot/src/main/java/com/example/demo/ReStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReStoreApplication.class, args);
+	}
+
+}
